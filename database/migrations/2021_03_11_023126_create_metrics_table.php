d5e237662daeb1d8a2a3886377673ea4a0b45b36
Matt Jeffery <matt@mattjeffery.dev>
2021-03-17 19:56:35 -0500

    Remove min id

--- database/migrations/2021_03_11_023126_create_metrics_table.php
+++ database/migrations/2021_03_11_023126_create_metrics_table.php
@@ -8,7 +8,7 @@ class CreateMetricsTable extends Migration
 {
     public function up() {
         Schema::create('metrics', function (Blueprint $table) {
-            $table->id()->from(100000);
+            $table->id(); //->from(100000);
             $table->bigInteger('user_id');
             $table->text('metric');
             $table->text('units');
