c84a67b1dc4dbe91842c2ff7a206d76668796027
Matt Jeffery <matt@mattjeffery.dev>
2021-03-21 15:30:10 -0500

    Replace use of auth-card with application card

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <x-auth-card>
+    <x-card>
         <x-slot name="logo">
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
@@ -44,5 +44,5 @@
                 </x-button>
             </div>
         </form>
-    </x-auth-card>
+    </x-card>
 </x-guest-layout>
