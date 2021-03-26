0a171016e4f8324c81c2b52efb558c96e814a8fd
Matt Jeffery <matt@mattjeffery.dev>
2021-03-26 06:45:42 -0500

    Add common text formatting to layout

--- resources/views/components/app-content.blade.php
+++ resources/views/components/app-content.blade.php
@@ -1,7 +1,7 @@
 <div class="py-3">
     <div class="max-w-full mx-auto sm:px-2 lg:px-3">
         <div class="bg-white overflow-y-auto rounded-lg md:h-screen sm:min-h-0">
-            <div class="p-3 bg-white">
+            <div class="p-3 bg-white text-lg font-medium text-gray-700">
                 {{ $slot }}
             </div>
         </div>
