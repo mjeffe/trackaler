b285e6fdb9a34f55f8285a403ea19010f2e76f1f
Matt Jeffery <matt@mattjeffery.dev>
2021-04-06 17:34:05 -0500

    Refactor tracker to show message when no trackers

--- resources/views/tracker/index.blade.php
+++ resources/views/tracker/index.blade.php
@@ -1,13 +1,28 @@
 <x-app-layout>
+@if (session()->get('error'))
+    <x-alert-danger>{{ session()->get('error') }}</x-alert-danger>
+@endif
+
+@if (count($trackers))
     <div class="text-2xl text-gray-500">
         Your Trackers
     </div>
+
     <a href="{{ route('tracker.create') }}" aria-label="Add a Tracker">
         <x-button class="mt-3" aria-label="Add a tracker">Add a Tracker</x-button>
     </a>
 
-    @if (session()->get('error'))
-        <x-alert-danger>{{ session()->get('error') }}</x-alert-danger>
-    @endif
     @include('tracker.list')
+@else
+    <x-card width="full">
+        <div class="text-2xl text-gray-500">
+            Tracal is a simple tool that lets you set goals, track stuff toward
+            that goal, and graph your progress.
+        </div>
+
+        <a href="{{ route('tracker.create') }}" aria-label="Add a Tracker">
+            <x-button class="mt-3" aria-label="Add a tracker">Add a Tracker</x-button>
+        </a>
+    </x-card>
+@endif
 </x-app-layout>
--- resources/views/tracker/list.blade.php
+++ resources/views/tracker/list.blade.php
@@ -1,43 +1,39 @@
-@if (count($trackers))
-    @foreach ($trackers as $t)
-        <x-card width="md" class="mb-3">
-            <div>
-                <span class="font-bold">{{ Str::title($t->metric) }}</span> ({{ $t->units }})
-                @if (!empty($t->goal_value))
-                    <div x-data={}>
-                        <span class="font-bold">Goal: </span> {{ $t->goal_value }}
-                        @if ($t->goal_date)
-                            <span class="font-bold pl-4">By: </span>
-                            <span x-html="localDateStr({{ $t->goal_date->valueOf() }})"></span>
-                        @endif
-                    </div>
-                @endif
-            </div>
-            <div>
-                {{ $t->description }}
-            </div>
-            <a href="{{ route('metric.create', $t->id) }}">
-                <x-button class="h-5" aria-label="Track">Track</x-button>
-            </a>
+@foreach ($trackers as $t)
+    <x-card width="md" class="mb-3">
+        <div>
+            <span class="font-bold">{{ Str::title($t->metric) }}</span> ({{ $t->units }})
+            @if (!empty($t->goal_value))
+                <div x-data={}>
+                    <span class="font-bold">Goal: </span> {{ $t->goal_value }}
+                    @if ($t->goal_date)
+                        <span class="font-bold pl-4">By: </span>
+                        <span x-html="localDateStr({{ $t->goal_date->valueOf() }})"></span>
+                    @endif
+                </div>
+            @endif
+        </div>
+        <div>
+            {{ $t->description }}
+        </div>
+        <a href="{{ route('metric.create', $t->id) }}">
+            <x-button class="h-5" aria-label="Track">Track</x-button>
+        </a>
 
-            <a href="{{ route('reporter.graph', $t->id) }}">
-                <x-button class="h-5" aria-label="Graph">Graph</x-button>
-            </a>
+        <a href="{{ route('reporter.graph', $t->id) }}">
+            <x-button class="h-5" aria-label="Graph">Graph</x-button>
+        </a>
 
-            <a href="{{ route('reporter.metrics', $t->id) }}">
-                <x-button class="h-5" aria-label="Metrics">Data</x-button>
-            </a>
+        <a href="{{ route('reporter.metrics', $t->id) }}">
+            <x-button class="h-5" aria-label="Metrics">Data</x-button>
+        </a>
 
-            <a href="{{ route('tracker.edit', $t->id) }}">
-                <x-button class="h-5" aria-label="Edit this Tracker">Edit</x-button>
-            </a>
+        <a href="{{ route('tracker.edit', $t->id) }}">
+            <x-button class="h-5" aria-label="Edit this Tracker">Edit</x-button>
+        </a>
 
-            <a href="{{ route('tracker.delete', $t->id) }}">
-                <x-button class="h-5" aria-label="Delete this Tracker">Delete</x-button>
-            </a>
+        <a href="{{ route('tracker.delete', $t->id) }}">
+            <x-button class="h-5" aria-label="Delete this Tracker">Delete</x-button>
+        </a>
 
-        </x-card>
-    @endforeach
-@else
-    You have no trackers
-@endif
+    </x-card>
+@endforeach
