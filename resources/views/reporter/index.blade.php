0e03b4018e532bb1031dbd45b7885689bdb90ffd
Matt Jeffery <matt@mattjeffery.dev>
2021-04-07 07:44:23 -0500

    Remove tracker selector from graph view

--- resources/views/reporter/index.blade.php
+++ resources/views/reporter/index.blade.php
@@ -1,24 +1,11 @@
 <x-app-layout>
-    <div class="text-2xl text-gray-500">
-        Select the metric you want to view
-    </div>
-    <div x-data="{ selectedMetric: '{{ $trackers->first()->id }}' }">
-        <x-select id="metric" name="metric" x-model="selectedMetric">
-        @foreach ($trackers as $t)
-            <option value="{{ $t->id }}">{{ Str::title($t->metric) }}</option>
-        @endforeach
-        </x-select>
-
-        <a :href="'{{ url("reporter") }}/' + selectedMetric + '/graph'">
-            <x-button class="mt-3 ml-3" aria-label="Show">Show</x-button>
-        </a>
-    </form>
-
-    @if (count($tracker->metrics))
+@if (count($tracker->metrics))
     <x-card width="full">
         <div class="mt-3">
             @includeWhen(count($tracker->metrics), 'reporter.chart-line')
         </div>
     </x-card>
-    @endif
+@else
+    You have not tracked any data for the {{ Str::title($tracker->metric) }} tracker
+@endif
 </x-app-layout>
