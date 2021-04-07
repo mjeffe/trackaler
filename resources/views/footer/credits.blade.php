b7cbf9d89dae6afe745d9ab6d911cf45478d0b58
Matt Jeffery <matt@mattjeffery.dev>
2021-04-07 13:26:08 -0500

    Add tooltips classes and modify tracker edit

--- resources/views/footer/credits.blade.php
+++ resources/views/footer/credits.blade.php
@@ -9,6 +9,7 @@
             <li><x-a href="https://github.com/alpinejs/alpine" target="_blank">Alpine.js</x-a></li>
             <li><x-a href="https://iconmonstr.com" target="_blank">Icons by iconmonstr</x-a></li>
             <li><x-a href="https://www.highcharts.com" target="_blank">Highcharts</x-a></li>
+            <li><x-a href="https://github.com/Cosbgn/tailwindcss-tooltips" target="_blank">Tooltips</x-a></li>
         </ul>
     </x-card>
 </x-app-layout>
--- resources/views/tracker/list.blade.php
+++ resources/views/tracker/list.blade.php
@@ -2,6 +2,12 @@
     <x-card width="md" class="mb-3">
         <div>
             <span class="font-bold">{{ Str::title($t->metric) }}</span> ({{ $t->units }})
+
+            <a id="edit-tracker" href="{{ route('tracker.edit', $t->id) }}" class="float-right has-tooltip">
+                <span class="tooltip">Edit (or delete) this tracker</span>
+                <img src="{{ asset('img/iconmonstr-pencil-9.svg') }}">
+            </a>
+
             @if (!empty($t->goal_value))
                 <div x-data={}>
                     <span class="font-bold">Goal: </span> {{ $t->goal_value }}
@@ -25,10 +31,6 @@
 
         <a href="{{ route('reporter.metrics', $t->id) }}">
             <x-button class="h-5" aria-label="Metrics">Data</x-button>
-        </a>
-
-        <a href="{{ route('tracker.edit', $t->id) }}">
-            <x-button class="h-5" aria-label="Edit this Tracker">Edit</x-button>
         </a>
     </x-card>
 @endforeach
