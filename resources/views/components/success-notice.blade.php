68a5fc948103aacf564744b0cdbed724864bc5c8
Matt Jeffery <matt@mattjeffery.dev>
2021-04-18 17:17:44 -0500

    Add x-cloak to prevent items flashing on screen
    Started with the success message. On initial load, the success message
    or help messages can be seen briefly flashing on screen before js loads
    and hides them.  Alpine.js doc suggests adding the x-cloak

--- resources/views/components/success-notice.blade.php
+++ resources/views/components/success-notice.blade.php
@@ -1,13 +1,12 @@
 @if (Session::has('success'))
-<div {{ $attributes->merge(['class' => 'absolute transition-opacity w-full bg-green-400 text-white font-semibold text-md text-center rounded-md']) }}
+<div {{ $attributes->merge(['class' => 'absolute z-10 transition-opacity w-full bg-green-400 text-white font-semibold text-md text-center rounded-md']) }}
         x-data="{showBadge: true}"
-        x-init="setTimeout(function () { showBadge = false; }, 800)"
+        x-init="setTimeout(function () { showBadge = false; }, 300)"
+        x-cloak
         x-show="showBadge"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
         x-transition:leave="ease-in duration-500"
-        x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0"
 >
     <x-img.check-mark class="w-4 h-4 mr-2 inline"/>Success!
