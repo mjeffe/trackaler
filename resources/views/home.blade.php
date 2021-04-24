9429d8e867bb9f3fa46a159f693f4370979ca57b
Matt Jeffery <matt@mattjeffery.dev>
2021-04-24 13:08:17 -0500

    Add login/register links to home landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,19 @@
         <x-page-title>
             Tracle is a simple tool that lets you set goals, track stuff toward that goal, and graph your progress.
         </x-page-title>
+
+        <div class="flex items-center justify-around md:justify-start w-full mt-3">
+            <div class="md:m-3">
+                <x-a href="{{ route('login') }}">
+                    {{ __('Login') }}
+                </x-a>
+            </div>
+            <div class="md:m-3">
+                <x-a href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </x-a>
+            </div>
+        </div>
     </x-card>
+
 </x-app-layout>
