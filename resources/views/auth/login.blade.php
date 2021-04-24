f4387742dc605e0e1e1836403c4593d2c8ed57c0
Matt Jeffery <matt@mattjeffery.dev>
2021-04-24 10:57:42 -0500

    Replace obsolete x-application-logo with new x-img
    The auth views were still using the old application logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-card>
         <x-slot name="title">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-img.app-logo class="w-6 h-6"/>
             </a>
         </x-slot>
 
