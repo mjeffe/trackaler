b10f99680b2d19979a456cc859ea19285ec98a96
Matt Jeffery <matt@mattjeffery.dev>
2021-04-24 11:21:23 -0500

    Fix card component width bug... again!

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,7 @@
 @props(['width' => 'md'])
 
 <div {{ $attributes->merge(['class' => 'min-h-full flex flex-col sm:justify-center items-center pt-6 sm:pt-0']) }}>
-    <div class="w-full max-w-md md:max-w-{{ $width }} px-6 py-4 bg-primary-200 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-{{ $width }} px-6 py-4 bg-primary-200 shadow-md overflow-hidden sm:rounded-lg">
         @if (!empty($title))
             <div class="text-2xl text-gray-500 mb-6">
                 {{ $title }}
