c3b110e133ba49d00793393084ef844117ec612e
Matt Jeffery <matt@mattjeffery.dev>
2021-05-02 14:54:23 -0500

    Add maintenance mode view

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <x-card width="full">
+        <x-page-title>
+            {{ config('app.name') }} is down for maintenance
+        </x-page-title>
+
+        <p class="mt-3 text-gray-500">
+            We should be back up shortly!
+        </p>
+    </x-card>
+
+</x-app-layout>
