55becbf980738ff448ddf1179d2d026bd7d0a6ea
Matt Jeffery <matt@mattjeffery.dev>
2021-05-02 14:54:54 -0500

    Replace hard coded app name I missed earlier

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
     <x-card width="full">
         <x-page-title>
-            Tracle is a simple tool that lets you set goals, track stuff toward that goal, and graph your progress.
+            {{ config('app.name') }} is a simple tool that lets you set goals,
+            track steps toward that goal, and graph your progress.
         </x-page-title>
+
+        <p class="mt-3 text-gray-500">
+            It's free, open source, and (currently) self-funded, so no pesky adds or email.
+        </p>
 
         <div class="flex items-center justify-around md:justify-start w-full mt-3">
             <div class="md:m-3">
