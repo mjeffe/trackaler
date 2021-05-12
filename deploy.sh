13c52d8c4a24e3d38ba18f491ab7ec883bbd3a26
Matt Jeffery <matt@mattjeffery.dev>
2021-05-12 12:02:35 -0500

    Add new cicd workflow and post deploy.sh script
    I'm abandoning my attempt at independent test/build/deploy workflows.
    GitHub Actions is just not mature enough to work this way. The current
    commit combines them into a single new cicd.yml workflow and dissables
    the others. It is still not an ideal CI/CD solution but seems to be
    adequate for now.

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# I've been experimenting with GitHub Actions for CI/CD, so this is more like a
+# "post" deploy script for the GHA deploy stuff.
+#
+# See notes in the head of .github/workflows/cicd.yml
+#
+
+# default to staging
+export DEPLOY_TO=${1:=stg}
+
+export PATH="$HOME/bin:$PATH"
+PHP=`which php-8.0`
+COMPOSER=`which composer-8.0`
+
+if [ "$PHP"x = 'x' ]; then
+    echo "Unable to locate php binary"
+    exit 1
+fi
+if [ "$COMPOSER"x = 'x' ]; then
+    echo "Unable to locate composer binary"
+    exit 1
+fi
+
+$PHP $COMPOSER install
+$PHP artisan migrate
+$PHP artisan config:cache
+$PHP artisan route:cache
+$PHP artisan view:cache
+
+if [ $DEPLOY_TO = 'stg' ]; then
+    $PHP artisan db:seed
+fi
+
+
