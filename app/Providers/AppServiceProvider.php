84a724e094efef1e4cd84bdb2b2da1f3b85237c3
Matt Jeffery <matt@mattjeffery.dev>
2021-05-21 16:08:52 -0500

    Override backup encryption event trigger with custom
    The even trigger was still checking the package listener for
    shouldEncrypt() which was then not triggering the event. This overrides
    the listener trigger so it checks our custom listener's shouldEncrypt().

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Listeners\EncryptBackupArchive as OverrideEncryptBackupArchive;
+use Spatie\Backup\Listeners\EncryptBackupArchive;
+use Spatie\Backup\Events\BackupZipWasCreated;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,10 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function register() {
         // override the spatie/laravel-backup package's encryption listener
         // ! NOTE !  I would not recommend using this! See notes in the listener
-        $this->app->bind(
-            \Spatie\Backup\Listeners\EncryptBackupArchive::class,
-            \App\Listeners\EncryptBackupArchive::class
-        );
+        $this->app->bind(EncryptBackupArchive::class, OverrideEncryptBackupArchive::class);
     }
 
     /**
@@ -25,8 +26,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+    public function boot() {
+        // override the spatie/laravel-backup package's event trigger
+        // ! NOTE !  I would not recommend using this! See notes in the listener
+        if (OverrideEncryptBackupArchive::shouldEncrypt()) {
+            Event::listen(BackupZipWasCreated::class, OverrideEncryptBackupArchive::class);
+        }
     }
 }
