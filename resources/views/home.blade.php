e1d022ec6732c3e8ef527f3dac85f387fa9ea2ee
Matt Jeffery <matt@mattjeffery.dev>
2021-05-28 16:40:55 -0500

    Add some app screenshots to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,54 @@
             track steps toward that goal, and graph your progress.
         </x-page-title>
 
-        <p class="mt-3 text-gray-500">
+        <p class="text-gray-500">
             It's free, open source, and (currently) self-funded, so no pesky adds or email.
         </p>
+
+        <!-- div class="flex items-center justify-around md:justify-start w-full mt-3" -->
+        <div class="w-full mt-3">
+
+            <div class="flex flex-col lg:flex-row items-center justify-evenly mt-3">
+                <div class="max-w-lg mx-auto">
+                    <div class="text-4xl font-extrabold text-gray-700 mb-6">
+                        Create a tracker
+                    </div>
+                </div>
+                <div class="md:m-3">
+                    <img src="{{ asset('img/screenshots/create-tracker.png') }}">
+                </div>
+            </div>
+            <div class="flex flex-col lg:flex-row items-center justify-evenly mt-3">
+                <div class="max-w-lg mx-auto">
+                    <div class="text-4xl font-extrabold text-gray-700 mb-6">
+                        Interact with your trackers
+                    </div>
+                </div>
+                <div class="md:m-3">
+                    <img src="{{ asset('img/screenshots/home-screen.png') }}">
+                </div>
+            </div>
+            <div class="flex flex-col lg:flex-row items-center justify-evenly mt-3">
+                <div class="max-w-lg mx-auto">
+                    <div class="text-4xl font-extrabold text-gray-700 mb-6">
+                        Track by adding data points to your trackers
+                    </div>
+                </div>
+                <div class="md:m-3">
+                    <img src="{{ asset('img/screenshots/add-metric.png') }}">
+                </div>
+            </div>
+            <div class="flex flex-col lg:flex-row items-center justify-evenly mt-3">
+                <div class="max-w-lg mx-auto">
+                    <div class="text-4xl font-extrabold text-gray-700 mb-6">
+                        Graph your trackers
+                    </div>
+                </div>
+                <div class="md:m-3">
+                    <img src="{{ asset('img/screenshots/graph-tracker.png') }}">
+                </div>
+            </div>
+        </div>
 
         <div class="flex items-center justify-around md:justify-start w-full mt-3">
             <div class="md:m-3">
